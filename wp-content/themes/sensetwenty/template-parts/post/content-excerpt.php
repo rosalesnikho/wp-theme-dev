5f1fff9bfa3045a82eaaa9813464f43bc1442cc1
nikvicros <nikho.rosales@gmail.com>
2020-03-11 11:11:00 -0500

    refactored template parts

--- wp-content/themes/sensetwenty/template-parts/post/content-excerpt.php
+++ wp-content/themes/sensetwenty/template-parts/post/content-excerpt.php
@@ -4,9 +4,9 @@
 	if ( has_post_thumbnail() ) {
 		?>
         <div class="entry-image">
-            <a href="<?php the_permalink();?>">
+            <a href="<?php the_permalink(); ?>">
 				<?php the_post_thumbnail( 'full', [
-				        // adds class to the image thumbnail
+					// adds class to the image thumbnail
 					'class' => 'image_fade'
 				] ); ?>
             </a>
@@ -16,32 +16,35 @@
 	?>
     <div class="entry-title">
         <h2>
-            <a href="<?php the_permalink();?>">
-                <?php the_title(); ?>
+            <a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
             </a>
         </h2>
     </div>
     <ul class="entry-meta clearfix">
-        <li><i class="icon-calendar3"></i> <?php echo get_the_date()?></li>
+        <li><i class="icon-calendar3"></i> <?php echo get_the_date() ?></li>
         <li>
-            <a href="<?php the_permalink(); ?>">
-                <i class="<?php echo get_the_author_link(get_the_author_meta('ID'))?>"></i>
-                <?php the_author();?>
+            <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+                <i class="icon-user"></i>
+				<?php the_author(); ?>
             </a>
         </li>
         <li>
+            <a>
+
+            </a>
             <i class="icon-folder-open"> </i>
-	        <?php the_category(' ');?>
+			<?php the_category( ' ' ); ?>
         </li>
         <li>
-            <a href="<?php the_permalink();?>">
+            <a href="#">
                 <i class="icon-comments"></i>
-                <?php comments_number('No comment');?>
+				<?php comments_number( 'No comment' ); ?>
             </a>
         </li>
     </ul>
     <div class="entry-content">
-        <?php the_excerpt();?>
-        <a href="<?php the_permalink();?>" class="more-link">Read More</a>
+		<?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>" class="more-link">Read More</a>
     </div>
 </div>
